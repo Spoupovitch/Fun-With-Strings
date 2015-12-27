9b32afb893163803f8247c10d5aa726b5fbaf396
Trevor <trevormjenkins@yahoo.com>
2015-12-26 23:28:29 -0500

    finish shiftDown and firstUnique functions

--- StringFun.c
+++ StringFun.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void reverseString(char *str);
+void firstUnique(char *str);
 
 int main (int *argc, char **argv) {
 	int cmd = 0;
@@ -33,7 +34,7 @@ int main (int *argc, char **argv) {
 			case 0: 
 				printf("\nPlease enter one of the following commands:\n");
 				printf("\t1. Print String\n\t2. Reverse String\n");
-				printf("\t3. Check \n\t4. \n");
+				printf("\t3. 1st Unique \n\t4. \n");
 				printf("\t9. Exit\n\t0. Command List\n\n");
 			break;
 			
@@ -60,4 +61,40 @@ void reverseString(char *str) {
 	//str++ screwing up recursive call
 	reverseString(str + 1);
 	printf("%c", str[0]);
+}
+
+//create second array in alphabetical order
+void firstUnique(char *str) {
+	int len = strlen(str);
+	char swap;
+	char *word = 
+	
+	for (i = 0; i < len - 1; i++) {
+		
+		//2nd letter in str[] later than 1st
+		if (str[i] - str[i + 1] < 0) {
+			
+			//last index of word[] earlier than str[]
+			if (word[len] - str[i + 1] < 0) {
+				shiftDown(word, len);
+				word[len] = str[i + 1];
+			}
+		}
+			
+	}
+}
+
+void shiftDown(char *str, int n) {
+	int low = n;
+	
+	//move low index down until a NULL is found
+	while (str[low - 1] != NULL && low > 0) {
+		low--;
+	}
+	
+	//shift all chars down an index
+	while (low <= n) {
+		str[low - 1] = str[low];
+		low++;
+	}
 }
