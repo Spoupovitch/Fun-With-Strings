f0e8e09c6399c4ca07f33a48ce9588464cbfb7b3
Trevor <trevormjenkins@yahoo.com>
2016-01-11 04:06:10 -0500

    added catch cases for second argument. remove test printouts from firstUnique.

--- StringFun.c
+++ StringFun.c
@@ -6,23 +6,39 @@ void reverseString(char *str);
 char firstUnique(char *str);
 void bubbleSort(char *str, int len);
 void swap(char *str, int a, int b);
+void checkAnagram(char *str1, char *str2);
 
 int main (int *argc, char **argv) {
 	int cmd = 0;
+	int secondArg;
 	char unique;
 	
+	//catch no argument given
 	if (argv[1] == NULL || argv[1] == "") {
 		printf("Try again, stupid. Enter a working string to the command line.\n");
 		return 0;
 	}
 	
-	printf("\nGot it, your working string is: %s\n", argv[1]);
+	//catch second argument or no second argument
+	if (argv[2] == NULL || argv[2] == "") {
+		printf("\nGot it, your working string is: %s\n", argv[1]);
+		secondArg = 0;
+	}
+	else {
+		printf("\nOk, your working strings are: %s, %s\n", argv[1], argv[2]);
+		secondArg = 1;
+	}
 	
 	while (cmd != 9) {
 		
 		switch (cmd) {
 			case 1:
-				printf("\t-%s\n\n", argv[1]);
+				if (!secondArg) {
+					printf("\t-%s\n\n", argv[1]);
+				}
+				else {
+					printf("\t-%s -%s\n\n", argv[1], argv[2]);
+				}
 			break;
 			
 			case 2:
@@ -37,9 +53,9 @@ int main (int *argc, char **argv) {
 			break;
 			
 			case 4:
-        			checkAnagram(argv[1], argv[2]);
-                		printf("\n\n");
-            		break;
+				checkAnagram(argv[1], argv[2]);
+				printf("\n\n");
+				break;
 			
 			case 0: 
 				printf("\nPlease enter one of the following commands:\n");
@@ -49,16 +65,16 @@ int main (int *argc, char **argv) {
 			break;
 			
 			default:
-			;
+				printf("Please enter a valid command, enter 0 to see the menu.\n\n");
 		}
 		
-		//cmd initialized to 0 will print prompt, cmd need only be changed
-		//outside of switch() after first call of while(). saves n lines
-		//of code where n is number of lines of prompt.
+		/*cmd initialized to 0 will print prompt, cmd need only be changed
+		outside of switch() after first call of while(). saves n lines
+		of code where n is number of lines of prompt.*/
 		scanf("%d", &cmd);
 	}
 	
-	printf("\n---------------Bye, bitch!---------------\n");
+	printf("\n---------------Bye!---------------\n");
 	
 	return 0;
 }
@@ -85,8 +101,6 @@ char firstUnique(char *str) {
 	
 	//word[] = sorted version of str[]
 	bubbleSort(word, len);
-	
-	printf("str: %s\nword: %s\n", str, word);
 	
 	//traverse original string in order
 	for (i = 0; i < len; i++) {
@@ -139,33 +153,41 @@ void swap(char *str, int a, int b) {
 	str[b] = temp;
 }
 
-void checkAnagram(char *first, char *second){
-    int i;
-    int len1 = strlen(first);
-    int len2 = strlen(second);
-    //each index of alpha[] represents its respective letter
-    int alpha[26] = { 0 };
+void checkAnagram(char *str1, char *str2) {
+	int i;
+	int len1 = strlen(str1);
+	int len2 = strlen(str2);
+	//each index of alpha[] represents its respective letter of the alphabet
+	int alpha[26] = { 0 };
 
-    //edge case
-    if (len1 != len2) {
-        printf("No match, faggot.\n");
-        return;
-    }
+	//edge case
+	if (len1 != len2) {
+		printf("\t-These strings have different lengths, no match.\n");
+		return;
+	}
+	
+	//edge case
+	if (str2 == NULL || str2 == "") {
+		printf("\t-No second string to compare.");
+		return;
+	}
+	
+	//increment alpha[] indices that match ascii values of str1
+	for (i = 0; i < len1; i++) {
+		alpha[str1[i] - 'a']++;
+	}
 
-    for (i = 0; i < len2; i++) {
-       alpha[first[i] - 'a']++;
-    }
-
-    for (i = 0; i < len1; i++) {
-        alpha[second[i] - 'a']--;
-    }
-
+	//decrement alpha[] indices that match ascii values of str2
+	for (i = 0; i < len2; i++) {
+		alpha[str2[i] - 'a']--;
+	}
+	
+	//if anagrams, all indices of alpha[] should now be 0
     for (i = 0; i < 26; i++) {
-
-        if(alpha[i] != 0) {
-            printf("No math, gayboy.\n");
-            return;
-        }
-    }
-    printf("These two strings are anagrams!\n");
+		if (alpha[i] != 0) {
+			printf("\t-These two don't match!\n");
+			return;
+		}
+	}
+	printf("\t-These two strings are anagrams!\n");
 }
